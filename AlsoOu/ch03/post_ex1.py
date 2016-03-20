503fa85732a3e3e819d5bfac729ef66205d0801e
gsrr <jerrycheng1128@gmail.com>
2016-03-21 06:29:15 +0800

    update

--- AlsoOu/ch03/post_ex1.py
+++ AlsoOu/ch03/post_ex1.py
@@ -1,13 +1,12 @@
-#http://stackoverflow.com/questions/27835619/ssl-certificate-verify-failed-error
+
 import urllib
 import urllib2
-import ssl
 
-url = "https://gcis.nat.gov.tw/pub/cmpy/cmpyInfoListAction.do"
+url = "http://gcis.nat.gov.tw/pub/cmpy/cmpyInfoListAction.do"
 
 params = {
     "brBanNo" : "",
-    "imageCode" : "458895",
+    "imageCode" : "020189",
     "isShowEUC" : "N",
     "method" : "query",
     "otherEnterFlag" : "false",
@@ -23,7 +22,7 @@ headers = {
         "Accept-Language" : "en-US,en;q=0.5",
         "Cache-Control" : "max-age=0",
         "Connection" : "keep-alive",
-        "Cookie" : "JSESSIONID=621b938194f3633aeb7fa0aa2829; _ga=GA1.3.1701390504.1455419117",
+        "Cookie" : "JSESSIONID=114ba2f3080eb0353e14168f7ac2; _ga=GA1.3.1701390504.1455419117",
         "Host" : "gcis.nat.gov.tw",
         "Referer" : "https://gcis.nat.gov.tw/pub/cmpy/cmpyInfoListAction.do",
         "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0"
@@ -31,8 +30,7 @@ headers = {
 }
 params = urllib.urlencode(params)
 req = urllib2.Request(url, params, headers)
-gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-response = urllib2.urlopen(req, context=gcontext)
+response = urllib2.urlopen(req)
 data = response.read()
 data = data.decode("big5").encode("utf-8")
 print data
